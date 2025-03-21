12e76631cb5150964f88669a68c6bf452234b826
Julia Macena <julia_fm_silva@estudante.sesisenai.org.br>
2025-03-20 21:37:14 -0300

    Aula 01

---
+++ android/app/src/main/kotlin/com/example/armazenamento_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.armazenamento_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/user_settings_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserSettingsScreen extends StatefulWidget {
+  const UserSettingsScreen({super.key});
+
+  @override
+  State<UserSettingsScreen> createState() => _UserSettingsScreenState();
+
+}
+
+class _UserSettingsScreenState extends State<UserSettingsScreen> {  
+    static const String userNameKey = 'username';
+    static const String userAgeKey = 'user_age';
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Configurações do Usuário")),
+      body: _buildUserSettingsScreenBody()
+    );
+  }
+  void _loadUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _nameController.text = prefs.getString(userNameKey) ?? "";
+      _ageController.text = prefs.getInt(userAgeKey)?.toString() ?? "";
+    });
+        ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text('Dados Carregados!')),
+      
+    );
+    }
+  
+  void _saveUserData() async {
+    String username = _nameController.text;
+    int age = int.tryParse(_ageController.text) ?? 0;
+
+    final preferences = await SharedPreferences.getInstance();
+
+    await preferences.setInt(userAgeKey, age);
+    await preferences.setString(userNameKey, username);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text('Dados Salvos com Sucesso!')),
+    );
+
+}
+
+   _buildUserSettingsScreenBody() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(labelText: 'Nome'),
+          ),
+          TextField(
+            controller: _ageController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Idade'),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+               onPressed: _loadUserData,
+                style: ElevatedButton.styleFrom(
+                 backgroundColor: Color(0xFF1877F2),
+                foregroundColor: Colors.white,
+                ),
+                   child: Text('Salvar'),
+             ),
+             ElevatedButton(
+              onPressed: _loadUserData,
+                style: ElevatedButton.styleFrom(
+                 backgroundColor: Color(0xFF1877F2),
+                foregroundColor: Colors.white,
+                ),
+                   child: Text('Carregar'),
+             ),
+            ],
+          )
+        ],
+    )
+    );
+  }
+}
+
